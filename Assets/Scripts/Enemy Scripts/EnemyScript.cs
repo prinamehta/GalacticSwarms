205e937add356f7d7022d9512032fc61a5d0758f
jeel11p <jeel11patel@gmail.com>
2025-03-30 22:08:31 -0700

    Revert "Added particle explosion on collision with enemy"
    This reverts commit 27fb0feef744d4e68197701c7a2d7cf5b0292cd7.

--- Assets/Scripts/Enemy Scripts/EnemyScript.cs
+++ Assets/Scripts/Enemy Scripts/EnemyScript.cs
@@ -4,7 +4,6 @@ public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private GameObject enemyBullet;
     [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject explosionEffect;
     public float minFireRate = 3f, maxFireRate = 6f;
     public float startFireDelay = 3f;
     private float fireTimer;
@@ -53,20 +52,10 @@ public class EnemyScript : MonoBehaviour
                 Destroy(other.gameObject);
                 return;
             }
-            TriggerExplosion();
             Destroy(other.gameObject); 
             DestroyAllEnemyBullets();
             Destroy(gameObject);
         }
-    }
-
-    void TriggerExplosion()
-    {
-        if(explosionEffect!= null)
-        {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
-        }
-        
     }
 
     void DestroyAllEnemyBullets()
